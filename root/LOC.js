98e5762c4efc82bf9d7672ab1df49df714b4607f
an-alch3mist <83577810+an-alch3mist@users.noreply.github.com>
2025-01-14 03:51:33 +0600

    Update LOC.js
    removed prefix 0s

--- root/LOC.js
+++ root/LOC.js
@@ -5,31 +5,38 @@ let LOC =
 		{ 	val: 2024,
 			FILES: 
 			[
-				{ day:  "3", time:0050, done: 2, desc: "mul-it-over" },
-				{ day:  "4", time:5400, done: 2, desc: "xmas" },
-				{ day:  "5", time:0150, done: 2, desc: "print-queue" },
-				{ day:  "6", time:1200, done: 2, desc: "guard-gallivant" },
-				{ day:  "7", time:7000, done: 2, desc: "bridge-repair" },
-				{ day:  "8", time:0180, done: 2, desc: "antinode" },
-				{ day:  "9", time:0360, done: 2, desc: "disk-frag" },
-				{ day: "10", time:0000, done: 0, desc: "...." },
-				{ day: "11", time:0000, done: 0, desc: "...." },
-				{ day: "12", time:0000, done: 0, desc: "...." },
-				{ day: "13", time:0000, done: 0, desc: "...." },
-				{ day: "14", time:0000, done: 0, desc: "...." },
-				{ day: "15", time:0000, done: 0, desc: "...." },
-				{ day: "16", time:0000, done: 0, desc: "...." },
-				{ day: "17", time:0000, done: 0, desc: "...." },
-				{ day: "18", time:0000, done: 0, desc: "...." },
-				{ day: "19", time:0000, done: 0, desc: "...." },
-				{ day: "20", time:0000, done: 0, desc: "...." },
-				{ day: "21", time:0000, done: 0, desc: "...." },
-				{ day: "22", time:0000, done: 0, desc: "...." },
-				{ day: "23", time:0000, done: 0, desc: "...." },
-				{ day: "24", time:0000, done: 0, desc: "...." },
-				{ day: "25", time:0000, done: 0, desc: "...."	},
-			],
+
+				{ day:  "3", time:   50, done: 2, desc: "mul-it-over" },
+				{ day:  "4", time: 5400, done: 2, desc: "xmas" },
+				{ day:  "5", time:  150, done: 2, desc: "print-queue" },
+				{ day:  "6", time: 1200, done: 2, desc: "guard-gallivant" },
+				{ day:  "7", time: 7000, done: 2, desc: "bridge-repair" },
+				{ day:  "8", time:  180, done: 2, desc: "antinode" },
+				{ day:  "9", time:  360, done: 2, desc: "disk-frag" },
+				{ day: "10", time:  000, done: 0, desc: "...." },
+				{ day: "11", time:  000, done: 0, desc: "...." },
+				{ day: "12", time:  000, done: 0, desc: "...." },
+				{ day: "13", time:  000, done: 0, desc: "...." },
+				{ day: "14", time:  000, done: 0, desc: "...." },
+				{ day: "15", time:  000, done: 0, desc: "...." },
+				{ day: "16", time:  000, done: 0, desc: "...." },
+				{ day: "17", time:  000, done: 0, desc: "...." },
+				{ day: "18", time:  000, done: 0, desc: "...." },
+				{ day: "19", time:  000, done: 0, desc: "...." },
+				{ day: "20", time:  000, done: 0, desc: "...." },
+				{ day: "21", time:  000, done: 0, desc: "...." },
+				{ day: "22", time:  000, done: 0, desc: "...." },
+				{ day: "23", time:  000, done: 0, desc: "...." },
+				{ day: "24", time:  000, done: 0, desc: "...." },
+				{ day: "25", time:  000, done: 0, desc: "...."	},
+			], 
 		},
 
 	],
 };
+
+
+/*
+	avoid leading 0s
+	Octal literals now need to be prefixed with 0o (e.g., 0o50) to explicitly indicate octal.
+*/
