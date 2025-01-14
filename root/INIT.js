2260c529b92d47f376f26dec2ddd82d2b56df6c6
an-alch3mist <83577810+an-alch3mist@users.noreply.github.com>
2025-01-14 21:36:11 +0600

    Add files via upload
    INIT  // update for . time
    static/style.css update for inactive

--- root/INIT.js
+++ root/INIT.js
@@ -1,41 +1,40 @@
 function _A()
 {
+	console.log("Advent", LOC);
 	for(let year of LOC.YEAR)
 		for(let file of year.FILES)
 		{
-			/*let a = document.createElement("a");
-			a.setAttribute("class", "link");
-			a.setAttribute("target", "blank");
-
-			let span_desc = document.createElement("span");
-			span_desc.setAttribute("class", "desc");
-
-			let file_time = file.time.toString().padStart(4, ' ');
-			span_desc.innerHTML = `${file_time} .... <l>${file.desc}</l> ${file.day}`;
-
-			// ./2024-1/Day-4-xmas/index-xmas.html
-			a.setAttribute(
-				"href", 
-				`./${year.val}-1/Day-${file.day}-${file.desc}/index-${file.desc}.html`
-			);
-
-			a.appendChild(span_desc);
-			query(".link-holder").appendChild(a);*/
-
+	    	// a
 			let a = document.createElement("a");
 		    a.classList.add("link");
+
 		    a.setAttribute(
 				"href", 
 				`./${year.val}/Day-${file.day}-${file.desc}/index-${file.desc}.html`
 			);
+			a.setAttribute("target", "_blank");
 
+	    	// time
 		    let timeSpan = document.createElement("span");
 		    timeSpan.classList.add("time");
 		    timeSpan.innerHTML = file.time;
 
+	    	// desc
 		    let descSpan = document.createElement("span");
 		    descSpan.classList.add("desc");
 		    descSpan.innerHTML = `<l>${file.desc}</l> ${file.day}`;
+
+		    if(file.time == '.')
+		    {
+		    	// a
+				a.setAttribute("target", "_self");
+			    a.classList.add("link-inactive");
+			    a.setAttribute("href", `#`);
+		    	
+		    	// desc
+		    	descSpan.innerHTML = `${file.day}`;
+			   	descSpan.classList.add("desc-inactive");
+		    }
 
 		    a.appendChild(timeSpan);
 		    a.appendChild(descSpan);
@@ -43,25 +42,6 @@ function _A()
 		}
 }
 
-/*
-50 .... mul-it-over 3
-5400 .... xmas 4
-150 .... print-queue 5
-1200 .... guard-gallivant 6
-7000 .... bridge-repair 7
-180 .... antinode 8
-360 .... disk-frag 9
-
-
-50 	  .... mul-it-over 3
-5400  .... xmas 4
-150   .... print-queue 5
-1200  .... guard-gallivant 6
-7000  .... bridge-repair 7
-180   .... antinode 8
-360   .... disk-frag 9
-
-*/
 
 function query(str)
 {
--- root/LOC.js
+++ root/LOC.js
@@ -6,33 +6,42 @@ let LOC =
 			FILES: 
 			[
 
-				{ day:  "3", time:   50, done: 2, desc: "mul-it-over" },
-				{ day:  "4", time: 5400, done: 2, desc: "xmas" },
-				{ day:  "5", time:  150, done: 2, desc: "print-queue" },
-				{ day:  "6", time: 1200, done: 2, desc: "guard-gallivant" },
-				{ day:  "7", time: 7000, done: 2, desc: "bridge-repair" },
-				{ day:  "8", time:  180, done: 2, desc: "antinode" },
-				{ day:  "9", time:  360, done: 2, desc: "disk-frag" },
-				{ day: "10", time:  000, done: 0, desc: "...." },
-				{ day: "11", time:  000, done: 0, desc: "...." },
-				{ day: "12", time:  000, done: 0, desc: "...." },
-				{ day: "13", time:  000, done: 0, desc: "...." },
-				{ day: "14", time:  000, done: 0, desc: "...." },
-				{ day: "15", time:  000, done: 0, desc: "...." },
-				{ day: "16", time:  000, done: 0, desc: "...." },
-				{ day: "17", time:  000, done: 0, desc: "...." },
-				{ day: "18", time:  000, done: 0, desc: "...." },
-				{ day: "19", time:  000, done: 0, desc: "...." },
-				{ day: "20", time:  000, done: 0, desc: "...." },
-				{ day: "21", time:  000, done: 0, desc: "...." },
-				{ day: "22", time:  000, done: 0, desc: "...." },
-				{ day: "23", time:  000, done: 0, desc: "...." },
-				{ day: "24", time:  000, done: 0, desc: "...." },
-				{ day: "25", time:  000, done: 0, desc: "...."	},
+				{ day:  3, time: "  50", done: 2, desc: "mul-it-over" },
+				{ day:  4, time: "5400", done: 2, desc: "xmas" },
+				{ day:  5, time: " 150", done: 2, desc: "print-queue" },
+				{ day:  6, time: "1200", done: 2, desc: "guard-gallivant" },
+				{ day:  7, time: "7000", done: 2, desc: "bridge-repair" },
+				{ day:  8, time: " 180", done: 2, desc: "antinode" },
+				{ day:  9, time: " 360", done: 2, desc: "disk-frag" },
+				{ day: 10, time: " 120", done: 2, desc: "hoof-it" },
+				{ day: 11, time: " 500", done: 2, desc: "plutonian-pebbles" },
+				{ day: 12, time:    ".", done: 0, desc: "...." },
+				{ day: 13, time:    ".", done: 0, desc: "...." },
+				{ day: 14, time:    ".", done: 0, desc: "...." },
+				{ day: 15, time:    ".", done: 0, desc: "...." },
+				{ day: 16, time:    ".", done: 0, desc: "...." },
+				{ day: 17, time:    ".", done: 0, desc: "...." },
+				{ day: 18, time:    ".", done: 0, desc: "...." },
+				{ day: 19, time:    ".", done: 0, desc: "...." },
+				{ day: 20, time:    ".", done: 0, desc: "...." },
+				{ day: 21, time:    ".", done: 0, desc: "...." },
+				{ day: 22, time:    ".", done: 0, desc: "...." },
+				{ day: 23, time:    ".", done: 0, desc: "...." },
+				{ day: 24, time:    ".", done: 0, desc: "...." },
+				{ day: 25, time:    ".", done: 0, desc: "...." },
 			], 
 		},
 
+		{ 	val: 2023,
+			FILES: 
+			[
+				// { day: 1, time: "   .", done: 0, desc: "...." },
+				//
+			], 
+		},
 	],
+
+
 };
 
 
