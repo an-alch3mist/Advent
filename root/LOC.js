ef09f66ab365012b5cda7ec83603c30955635829
an-alch3mist <83577810+an-alch3mist@users.noreply.github.com>
2025-01-23 16:55:23 +0600

    Add files via upload

--- root/LOC.js
+++ root/LOC.js
@@ -23,7 +23,7 @@ let LOC =
 				{ day: "17", time:"  150", done: 2, desc: "chronospatial-computer" },
 				{ day: "18", time:"  150", done: 2, desc: "ram-run" },
 				{ day: "19", time:"  230", done: 2, desc: "linen-layout" },
-				{ day: "20", time:    ".", done: 0, desc: "...." },
+				{ day: "20", time:" 1100", done: 1, desc: "race-condition" },
 				{ day: "21", time:    ".", done: 0, desc: "...." },
 				{ day: "22", time:    ".", done: 0, desc: "...." },
 				{ day: "23", time:    ".", done: 0, desc: "...." },
